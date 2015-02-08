7a2dd956aaaa55b567c69ac953107d6ac89fbafe
lvrach <leo.al.vra@gmail.com>
2015-02-08 12:44:24 +0200

    Support pipe multiplexing

--- Pipe.js
+++ Pipe.js
@@ -1,32 +1,45 @@
-var Pipe = function () {
+
+var Stream = function (headers) {
 	this.events = {
-		'start': [],
 		'line': [], 
 		'end': [],
 		'close': []
 	};
+};
+Stream.prototype.on = function (event, callback) {
+	this.events[event].push(callback);
+};
+Stream.prototype.close = function () {
+	this.events.close.forEach(function (callback) {
+		callback();
+	});
+	delete this.events;
+};
+Stream.prototype.write = function (line) {
+	this.events.line.forEach(function (callback) {
+		callback(line);
+	});
+};
+Stream.prototype.end = function () {
+	this.events.end.forEach(function (callback) {
+		callback();
+	});
+};
+
+var Pipe = function () {
+	this.events = {
+		'start': []
+	};
+	this.streams = [];
 }
 Pipe.prototype.on = function (event, callback) {
 	this.events[event].push(callback);
 };
 Pipe.prototype.start = function (meta) {
+	var stream = new Stream(meta);
 	this.events.start.forEach(function (callback) {
-		callback(meta);
+		callback(meta, stream);
 	});
-};
-Pipe.prototype.close = function () {
-	this.events.close.forEach(function (callback) {
-		callback();
-	});
-};
-Pipe.prototype.write = function (line) {
-	this.events.line.forEach(function (callback) {
-		callback(line);
-	});
-};
-Pipe.prototype.end = function () {
-	this.events.end.forEach(function (callback) {
-		callback();
-	});
+	return stream;
 };
 module.exports = Pipe;
